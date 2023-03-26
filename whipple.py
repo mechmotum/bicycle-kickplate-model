efb3b8d2ff88654c1a27dd67197bde5d1caaa650
Jason K. Moore <moorepants@gmail.com>
2023-03-26 08:26:14 +0200

    Simulation runs with relaxation length equations.

--- whipple.py
+++ whipple.py
@@ -573,9 +573,9 @@ print(eval_dynamic(*[np.ones_like(a) for a in [qs, us, fs, rs, ps]]))
 
 def calc_y(t):
 
-    a = 0.1  # height
+    a = 0.4  # height
     b = -4.0  # shift to the right (neg val)
-    c = 10.0  # narrow the pulse
+    c = 20.0  # narrow the pulse
 
     # Gaussian error function exp(-x**2)
     y = a*np.exp(-(b + c*t)**2)
@@ -591,15 +591,15 @@ def rhs(t, x, p):
     ==========
     t : float
         Time value in seconds.
-    x : array_like, shape(16,)
+    x : array_like, shape(20,)
         State values where x = [q1, q2, q3, q4, q5, q6, q7, q8, u1, u2, u3, u4,
-        u5, u6, u7, u8].
+        u5, u6, u7, u8, Fry, Ffy, Mrz, Mfz].
     p : array_like, shape(28,)
         Constant values.
 
     Returns
     =======
-    xdot : ndarray, shape(16,)
+    xdot : ndarray, shape(20,)
         Time derivative of the state.
     force : ndarray, shape(2,)
         Ground normal force magnitudes at the rear and front wheel contacts.
@@ -610,6 +610,7 @@ def rhs(t, x, p):
 
     q = x[:8]
     u = x[8:16]
+    f = x[16:20]
 
     # steer, rear wheel, roll torques set to zero
     T4, T6, T7 = 0.0, 0.0, 0.0
@@ -621,11 +622,11 @@ def rhs(t, x, p):
 
     # This solves for the generalized accelerations and the normal forces at
     # the tire contact.
-    A, b = eval_dynamic(q, u, r, p)
+    A, b = eval_dynamic(q, u, f, r, p)
     # xplus = [us', Frz, Ffz]
     xplus = np.linalg.solve(A, b).squeeze()
 
-    return np.hstack((u, xplus[:8])), xplus[-2:]
+    return np.hstack((u, xplus[:12])), xplus[-2:]
 
 
 ########################
@@ -637,10 +638,10 @@ p_vals = {
    c_ar: 11.46,
    c_pf: 0.573,
    c_pr: 0.573,
-   c_maf: 0.0001,  # need real numbers for these
-   c_mar: 0.0001,
-   c_mpf: 0.0001,
-   c_mpr: 0.0001,
+   c_maf: 0.01,  # need real numbers for this
+   c_mar: 0.01,  # need real numbers for this
+   c_mpf: 0.01,  # need real numbers for this
+   c_mpr: 0.01,  # need real numbers for this
    d1: 0.9534570696121849,
    d2: 0.2676445084476887,
    d3: 0.03207142672761929,
@@ -667,6 +668,10 @@ p_vals = {
    mf: 3.0,
    rf: 0.35,
    rr: 0.3,
+   s_yf: 0.1,  # need real numbers for this
+   s_yr: 0.1,  # need real numbers for this
+   s_zf: 0.1,  # need real numbers for this
+   s_zr: 0.1,  # need real numbers for this
 }
 p_arr = np.array(list(p_vals.values()))
 
@@ -720,7 +725,11 @@ print('Initial dependent speeds (u1, u2, u5): ',
       u_vals[0], u_vals[1], np.rad2deg(u_vals[4]))
 print('Initial speeds: ', u_vals)
 
-initial_conditions = np.hstack((q_vals, u_vals))
+# initial tire forces
+# TODO : Need to figure out how we know the initial state of the tire forces.
+f_vals = np.array([0.0, 0.0, 0.0, 0.0])
+
+initial_conditions = np.hstack((q_vals, u_vals, f_vals))
 
 print('Test rhs with initial conditions and correct constants:')
 print(rhs(0.0, initial_conditions, p_arr))
@@ -750,9 +759,10 @@ holonomic_vs_time = eval_holonomic(x_traj[:, 4],  # q5
                                    p_vals[rr])
 
 deg = [False, False, True, True, True, True, True, True]
-fig, axes = plt.subplots(9, 2, sharex=True)
+fig, axes = plt.subplots(11, 2, sharex=True)
 q_traj = x_traj[:, :8]
-u_traj = x_traj[:, 8:]
+u_traj = x_traj[:, 8:16]
+f_traj = x_traj[:, 16:]
 fig.set_size_inches(8, 10)
 for i, (ax, traj, s, degi) in enumerate(zip(axes[:, 0], q_traj.T, qs, deg)):
     unit = '[m]'
@@ -760,19 +770,29 @@ for i, (ax, traj, s, degi) in enumerate(zip(axes[:, 0], q_traj.T, qs, deg)):
         traj = np.rad2deg(traj)
         unit = '[deg]'
     ax.plot(times, traj)
-    ax.set_ylabel(str(s) + ' ' + unit)
+    ax.set_ylabel(str(s) + '\n' + unit)
 for i, (ax, traj, s, degi) in enumerate(zip(axes[:, 1], u_traj.T, us, deg)):
     unit = '[m/s]'
     if degi:
         traj = np.rad2deg(traj)
         unit = '[deg/s]'
     ax.plot(times, traj)
-    ax.set_ylabel(str(s) + ' ' + unit)
+    ax.set_ylabel(str(s) + '\n' + unit)
+
+axes[8, 0].plot(times, f_traj[:, 0])
+axes[8, 0].set_ylabel(str(fs[0]) + '\n[N]')
+axes[9, 0].plot(times, f_traj[:, 1])
+axes[9, 0].set_ylabel(str(fs[1]) + '\n[N]')
+axes[8, 1].plot(times, f_traj[:, 2])
+axes[8, 1].set_ylabel(str(fs[2]) + '\n[N-m]')
+axes[9, 1].plot(times, f_traj[:, 3])
+axes[9, 1].set_ylabel(str(fs[3]) + '\n[N-m]')
+
 axes[-1, 0].plot(times, calc_y(times)[0])
-axes[-1, 0].set_ylabel('y [m]')
+axes[-1, 0].set_ylabel('y\n[m]')
 axes[-1, 0].set_xlabel('Time [s]')
 axes[-1, 1].plot(times, holonomic_vs_time)
-axes[-1, 1].set_ylabel('constraint [m]')
+axes[-1, 1].set_ylabel('constraint\n[m]')
 axes[-1, 1].set_xlabel('Time [s]')
 plt.tight_layout()
 plt.show()

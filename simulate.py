5e2d67450357a91798435c12f3f18b06b1324554
Jason K. Moore <moorepants@gmail.com>
2023-04-15 07:11:41 +0200

    Plot steer angle not roll angle.

--- simulate.py
+++ simulate.py
@@ -43,6 +43,12 @@ def calc_inputs(t, x, p):
     ku4 = -0.5939384880650549
     ku7 = 0.4340987861323103
 
+    # LQR gains for Whipple model at 3 m/s
+    #kq4 = -23.183400610625647
+    #kq7 = 17.086409893261113
+    #ku4 = -7.999852938163112
+    #ku7 = 1.8634394089384874
+
     T7 = -(kq4*q4 + kq7*q7 + ku4*u4 + ku7*u7)
 
     # kick plate force
@@ -323,10 +329,10 @@ plot_wheel_paths()
 # simplified figure
 # compare normal tire numbers and 10% change in slip coefficient
 # plot slip angle, lateral force for front and rear steer angle and force input
-def plot_minimal(t, q4, ar, af, fkp, fyr, fyf, axes=None, **kwargs):
+def plot_minimal(t, q7, ar, af, fkp, fyr, fyf, axes=None, **kwargs):
     if axes is None:
         fig, axes = plt.subplots(2, 1, sharex=True)
-    axes[0].plot(times, np.rad2deg(q4), color='C0', label=r'$\delta$',
+    axes[0].plot(times, np.rad2deg(q7), color='C0', label=r'$\delta$',
                  **kwargs)
     axes[0].plot(times, np.rad2deg(ar), color='C1', label=r'$\alpha_r$',
                  **kwargs)
@@ -350,7 +356,7 @@ initial_conditions = setup_initial_conditions(q_vals, u_vals, f_vals, p_arr)
 times, q_traj, u_traj, slip_traj, f_traj, fz_traj, con_traj, q9_traj, q10_traj = simulate(
     2.0, rhs, initial_conditions, p_arr, fps=1000)
 
-axes = plot_minimal(times, q_traj[:, 3], slip_traj[:, 0], slip_traj[:, 1],
+axes = plot_minimal(times, q_traj[:, 6], slip_traj[:, 0], slip_traj[:, 1],
                     calc_fkp(times), f_traj[:, 0], f_traj[:, 1])
 
 factor = 1.3

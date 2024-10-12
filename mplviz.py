da61ae85cb81321932506835bd5c095017f98de0
Jason K. Moore <moorepants@gmail.com>
2024-10-12 12:54:37 +0200

    Basic symmeplot animation working.

--- mplviz.py
+++ mplviz.py
@@ -1,43 +1,52 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from symbols import rr, rf, y, qs, ps
-from model import N, o, p, rear_wheel, front_wheel
+from symbols import rr, rf, y, qs, ps, d1, d2, d3
+from model import N, C, E, o, p, do, fo, nd, rear_wheel, front_wheel
 from symmeplot.matplotlib import Scene3D
-from base_simulation import p_arr, res
+from base_simulation import p_arr, res, fps
 
 (times, q_traj, u_traj, slip_traj, f_traj, fz_traj, con_traj, q9_traj,
  q10_traj, r_traj) = res
 
-scene = Scene3D(N, o, scale=0.5)
+fig, ax = plt.subplots(subplot_kw={'projection': '3d'})
+
+scene = Scene3D(N,
+                o.locatenew('e', nd.pos_from(o).dot(N.x)*N.x),
+                ax=ax, scale=2.0)
 
 scene.add_frame(N, p)  # kick plate
+scene.add_line([o, o.locatenew('pp', 0.5*N.x)])  # reference
+scene.add_line([p, p.locatenew('pp', 0.5*N.x)])  # kick plate
 
 rear_wheel_plot = scene.add_body(rear_wheel)
 rear_wheel_plot.attach_circle(
     rear_wheel.masscenter,
     rr,
     rear_wheel.frame.y,
-    facecolor="red", edgecolor="k")
+    facecolor="none", edgecolor="k")
 
 front_wheel_plot = scene.add_body(front_wheel)
 front_wheel_plot.attach_circle(
     front_wheel.masscenter,
     rf,
     front_wheel.frame.y,
-    facecolor="red", edgecolor="k")
+    facecolor="none", edgecolor="k")
 
-#scene.add_body(rear_frame_body)
-#scene.add_line(some_points_describing_the_rear_frame)
-#
-#scene.add_body(front_frame_body)
-#scene.add_line(some_points_describing_the_front_frame)
+scene.add_line([do,
+                do.locatenew('d1_half', d1*C.x),
+                fo.locatenew('d2_half', -d3*E.x - d2*E.z),
+                fo,
+                fo.locatenew('d3_half', -d3*E.x)])
 
-# For a single state plot:
 scene.lambdify_system(qs + [y] + list(ps))
 scene.evaluate_system(*np.hstack((q_traj[0, :], r_traj[0, 4:5], p_arr)))
 
-plt.show()
+scene.plot()
 
-#ani = scene.animate(lambda q: (q,), frames=np.linspace(0, 2 * np.pi, 60))
-#ani.save("animation.gif", fps=30)
+ax.invert_zaxis()
+
+slow_factor = 3  # int
+ani = scene.animate(lambda i: np.hstack((q_traj[i, :], r_traj[i, 4:5], p_arr)),
+                    frames=len(times), interval=slow_factor/fps*1000)
+ani.save("animation.gif", fps=fps//slow_factor)
